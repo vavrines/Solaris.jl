db2ae63a46cb6c45c3975ab0166fa25c7042bc30
vavrines <tianbaixiao@gmail.com>
2021-03-06 23:41:58 +0100

    Add icnn

--- test/test_layer.jl
+++ test/test_layer.jl
@@ -8,10 +8,21 @@ m = Chain(
     Solaris.Affine(32, 10),
 )
 
-m(randn(784))
+m(randn(Float32, 784))
 
 # regularization
 sqnorm(x) = sum(abs2, x)
 loss(x, y) = logitcrossentropy(m(x), y) + sum(sqnorm, Flux.params(m))
 
 loss(rand(Float32, 28^2), rand(Float32, 10))
+
+X = rand(Float32, 4, 100)
+Y = X.^2
+data = Flux.Data.DataLoader(X, Y, batchsize = 4, shuffle = true)
+
+m = Solaris.ICNN(4, 4, [10, 10, 10, 10], tanh)
+ps = params(m)
+loss(x, y) = sum(abs2, m(x) .- y) / 100
+cb = () -> println("loss: $(loss(X, Y))")
+
+Flux.@epochs 1 Flux.train!(loss, ps, data, ADAM(), cb = Flux.throttle(cb, 1))
