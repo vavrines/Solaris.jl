c019abec6d8d585eddf710a89ddf6239f74f64e0
vavrines <tianbaixiao@gmail.com>
2024-03-13 17:14:26 +0800

    Skip tensorflow test

--- test/test_io.jl
+++ test/test_io.jl
@@ -2,7 +2,7 @@ cd(@__DIR__)
 load_data("dataset.csv", dlm = ",")
 
 nn1 = load_model("model.jld2")
-nn2 = load_model("model.h5")
+#nn2 = load_model("model.h5")
 
 save_model(nn1)
-save_model(nn2)
+#save_model(nn2)
