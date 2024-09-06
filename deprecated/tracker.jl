8bd9597fc592b3c7b6cabe31307bc87da6aa5734
vavrines <tianbaixiao@gmail.com>
2024-09-06 08:46:22 +0800

    Deprecate tracker test

--- deprecated/tracker.jl
+++ deprecated/tracker.jl
@@ -23,3 +23,33 @@ function zygote_mode()
         Flux.Optimise.update!(x, -Flux.Optimise.apply!(opt, x, x̄))
     @eval Flux.gradient(f, args...) = Flux.Zygote.gradient(f, args...)
 end
+
+@testset "Widget" begin
+    x = randn(Float32, 10, 2)
+    y = rand(Float32, 1, 2)
+
+    model = Dense(10, 1) |> Solaris.track
+
+    function loss(x, y)
+        xs = Flux.unstack(x, 3)
+        ys = Flux.unstack(y, 3)
+        ŷs = model.(xs)
+        l = 0.0f0
+        for t = 1:length(ŷs)
+            l += Flux.mse(ys[t], ŷs[t])
+        end
+        return l / length(ŷs)
+    end
+    ps = Flux.params(model)
+    data = repeat([(x, y)], 10)
+    opt = Adam()
+    cb = () -> Flux.reset!(model)
+    Solaris.tracker_mode()
+    Flux.train!(loss, ps, data, opt, cb = cb)
+
+    ps1 = Flux.params(model)
+    Flux.train!(loss, ps1, data, opt, cb = cb)
+
+    model |> Solaris.untrack
+    Solaris.zygote_mode()
+end
