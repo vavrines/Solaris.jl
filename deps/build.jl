1ed30e62fd562a032470195e9ebb81b1157bedb3
vavrines <tianbaixiao@gmail.com>
2024-10-14 21:10:21 +0800

    Fix externally managed error

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,5 @@ using Conda
 #Conda.add_channel("conda-forge")
 #Conda.add("tensorflow")
 
-cmd = `pip3 install tensorflow --user`
-run(cmd)
+#cmd = `pip3 install tensorflow --user --break-system-packages`
+#run(cmd)
